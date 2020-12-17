14173d0697a97b52058cc175b036ca23f6f6c9c6
JamesLiao <vivlonglz@gmail.com>
2020-12-17 10:12:47 +0800

    update

--- src/Engine/aliyun.php
+++ src/Engine/aliyun.php
@@ -5,6 +5,8 @@ namespace PhalApi\Xsms\Engine;
 use AlibabaCloud\Client\AlibabaCloud;
 use AlibabaCloud\Client\Exception\ClientException;
 use AlibabaCloud\Client\Exception\ServerException;
+use AlibabaCloud\Dybaseapi\MNS\Requests\BatchDeleteMessage;
+use AlibabaCloud\Dybaseapi\MNS\Requests\BatchReceiveMessage;
 
 class Aliyun
 {
@@ -112,18 +114,16 @@ class Aliyun
      *
      * @return stdClass
      */
-    public function sendBatchSms($phoneNo, $sign, $tplCode, $tplParam = [], $extendCode = null)
+    public function sendBatchSms($phoneNoJson, $signNameJson, $tplCode, $tplParamJson, $extendCodeJson = null)
     {
         $params = [
-            'PhoneNumberJson' => json_encode($phoneNo, JSON_UNESCAPED_UNICODE),
-            'SignNameJson' => json_encode($sign, JSON_UNESCAPED_UNICODE),
+            'PhoneNumberJson' => json_encode($phoneNoJson, JSON_UNESCAPED_UNICODE),
+            'SignNameJson' => json_encode($signNameJson, JSON_UNESCAPED_UNICODE),
             'TemplateCode' => $tplCode,
+            'TemplateParamJson' => json_encode($tplParamJson, JSON_UNESCAPED_UNICODE),
         ];
-        if (!empty($tplParam)) {
-            $params['TemplateParamJson'] = json_encode($tplParam, JSON_UNESCAPED_UNICODE);
-        }
-        if (!empty($extendCode)) {
-            $params['SmsUpExtendCodeJson'] = json_encode($extendCode, JSON_UNESCAPED_UNICODE);
+        if (!empty($extendCodeJson)) {
+            $params['SmsUpExtendCodeJson'] = json_encode($extendCodeJson, JSON_UNESCAPED_UNICODE);
         }
 
         return $this->rpcRequest('SendBatchSms', $params);
@@ -147,5 +147,72 @@ class Aliyun
         }
 
         return $this->rpcRequest('SendBatchSms', $params);
+    }
+
+    /**
+     * 获取批量消息.
+     *
+     * @param string   $messageType 消息类型
+     * @param string   $queueName   在云通信页面开通相应业务消息后，就能在页面上获得对应的queueName<br/>(e.g. Alicom-Queue-xxxxxx-xxxxxReport)
+     * @param callable $callback    <p>
+     *                              回调仅接受一个消息参数;
+     *                              <br/>回调返回true，则工具类自动删除已拉取的消息;
+     *                              <br/>回调返回false,消息不删除可以下次获取.
+     *                              <br/>(e.g. function ($message) { return true; }
+     *                              </p>
+     */
+    public function receiveBatchMsg($messageType, $queueName, callable $callback)
+    {
+        $di = \PhalApi\DI();
+        $i = 0;
+        $params = [
+            'MessageType' => $messageType,
+            'QueueName' => $queueName,
+        ];
+        $response = null;
+        $token = null;
+        // 取回执消息失败3次则停止循环拉取
+        do {
+            try {
+                if (null == $token || strtotime($token['ExpireTime']) - time() > 2 * 60) {
+                    $response = $this->rpcRequest('QueryTokenForMnsQueue', $params);
+                }
+                $token = $response['data']['MessageTokenDTO'];
+                $mnsClient = new \AlibabaCloud\Dybaseapi\MNS\MnsClient(
+                    'http://1943695596114318.mns.cn-hangzhou.aliyuncs.com',
+                    $token['AccessKeyId'],
+                    $token['AccessKeySecret'],
+                    $token['SecurityToken']
+                );
+                $mnsRequest = new BatchReceiveMessage(10, 5);
+                $mnsRequest->setQueueName($queueName);
+                $mnsResponse = $mnsClient->sendRequest($mnsRequest);
+                $receiptHandles = [];
+                foreach ($mnsResponse->Message as $message) {
+                    $messageBody = base64_decode($message->MessageBody); // base64解码后的JSON字符串
+                    // 计算消息体的摘要用作校验
+                    $bodyMD5 = strtoupper(md5($messageBody));
+                    // 比对摘要，防止消息被截断或发生错误
+                    if ($bodyMD5 == $message->MessageBodyMD5) {
+                        // 执行回调
+                        if (call_user_func($callback, $messageBody)) {
+                            // 当回调返回真值时，删除已接收的信息
+                            $receiptHandles[] = $message->ReceiptHandle; // 加入$receiptHandles数组中的记录将会被删除
+                        }
+                    }
+                }
+                if (count($receiptHandles) > 0) {
+                    $deleteRequest = new BatchDeleteMessage($queueName, $receiptHandles);
+                    $mnsClient->sendRequest($deleteRequest);
+                }
+            } catch (ClientException $e) {
+                $di->logger->error(__NAMESPACE__.DIRECTORY_SEPARATOR.__CLASS__.DIRECTORY_SEPARATOR.__FUNCTION__, ['ClientException' => $e->getErrorMessage()]);
+            } catch (ServerException $e) {
+                if (404 == $e->getCode()) {
+                    ++$i;
+                }
+                $di->logger->error(__NAMESPACE__.DIRECTORY_SEPARATOR.__CLASS__.DIRECTORY_SEPARATOR.__FUNCTION__, ['ServerException' => $e->getErrorMessage()]);
+            }
+        } while ($i < 3);
     }
 }
